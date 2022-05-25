d1e9719cdb432bd61d25d1e9b5441ecd604704dc
ShavetaMalhotra <105014959+ShavetaMalhotra@users.noreply.github.com>
2022-05-25 23:53:49 +0530

    Control Statements

---
+++ ControlStatements/DecisionMakingStatements.java
@@ -0,0 +1,33 @@
+package ControlStatements;
+/*
+Selection/Decision Making Statements
+Loop/ Iteration Statements
+Branching/ Transfer Statements
+
+Selection/Decision Making Statements:
+1. if statement
+2. if-else statement
+3. ladder else if
+4. switch case
+
+ */
+public class DecisionMakingStatements {
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("if part...");
+        }
+
+        int a=10;
+
+        if(a>0){ //false
+            System.out.println("Value of a is 0");
+        }
+
+        a= 100;
+        if(a<0) {
+            System.out.println("Value of a is smaller than 0");
+            System.out.println("this is not a prt of if statement");
+        }
+
+    }
+}
